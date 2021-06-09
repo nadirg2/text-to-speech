29d03b5619d9b81da2ac4c94b5677c815fda7949
nadirg2 <gulievnadir3@gmail.com>
2021-06-09 14:32:03 +0300

    fix DOMContentLoaded and format code

--- js/main.js
+++ js/main.js
@@ -1,28 +1,30 @@
-var input = document.getElementById("input");
-var btn = document.getElementById("btn");
-var select = document.getElementById("voices");
-var synth = window.speechSynthesis;
-var voices = null;
+document.addEventListener("DOMContentLoaded", () => {
+  var input = document.getElementById("input");
+  var btn = document.getElementById("btn");
+  var select = document.getElementById("voices");
+  var synth = window.speechSynthesis;
+  var voices = null;
 
-fillVoices();
+  fillVoices();
 
-btn.addEventListener("click", () => {
-  var toSpeak = new SpeechSynthesisUtterance(input.value);
-  toSpeak.voice = voices[select.selectedIndex];
-  synth.speak(toSpeak);
-});
-
-function fillVoices() {
-  voices = synth.getVoices();
-  var selectedVoice = select.selectedIndex < 0 ? 0 : select.selectedIndex;
-  voices.forEach((voice) => {
-    let option = document.createElement("option");
-    let option_child = document.createElement("div");
-    option_child.setAttribute("class", "option__text");
-    option_child.textContent = voice.name;
-    option.appendChild(option_child);
-    option.setAttribute("data-name", voice.name);
-    select.appendChild(option);
+  btn.addEventListener("click", () => {
+    var toSpeak = new SpeechSynthesisUtterance(input.value);
+    toSpeak.voice = voices[select.selectedIndex];
+    synth.speak(toSpeak);
   });
-  select.selectedIndex = selectedVoice;
-}
+
+  function fillVoices() {
+    voices = synth.getVoices();
+    var selectedVoice = select.selectedIndex < 0 ? 0 : select.selectedIndex;
+    voices.forEach((voice) => {
+      let option = document.createElement("option");
+      let option_child = document.createElement("div");
+      option_child.setAttribute("class", "option__text");
+      option_child.textContent = voice.name;
+      option.appendChild(option_child);
+      option.setAttribute("data-name", voice.name);
+      select.appendChild(option);
+    });
+    select.selectedIndex = selectedVoice;
+  }
+});
